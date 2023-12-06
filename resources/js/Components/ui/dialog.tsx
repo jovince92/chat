5a1b4577884de027123b0b21515d85999c5aec6d
Vince <vince.sanity123@gmail.com>
2023-12-07 00:10:08 +0800

    build

--- resources/js/Components/ui/dialog.tsx
+++ resources/js/Components/ui/dialog.tsx
@@ -9,10 +9,11 @@ const Dialog = DialogPrimitive.Root
 const DialogTrigger = DialogPrimitive.Trigger
 
 const DialogPortal = ({
-  className,
+  //className,
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn(className)} {...props} />
+  <DialogPrimitive.Portal //className={cn(className)} 
+  {...props} />
 )
 DialogPortal.displayName = DialogPrimitive.Portal.displayName
 
